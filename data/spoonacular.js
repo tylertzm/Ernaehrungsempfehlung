5304ad73beb751faf1105c087189c3a6a2d931a3
tylertzm <TZM2002@protonmail.com>
2024-11-15 17:57:15 +0100

    tofu iron

--- data/spoonacular.js
+++ data/spoonacular.js
@@ -4,23 +4,26 @@ var Spoonacular = require('spoonacular');
 
 var defaultClient = Spoonacular.ApiClient.instance;
 // Configure API key authorization: apiKeyScheme
-var apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
-apiKeyScheme.apiKey = "6ec4ef0ad27443c593c8c9ba152ca71d"
-// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-//apiKeyScheme.apiKeyPrefix['x-api-key'] = "Token"
 
-var api = new Spoonacular.DefaultApi()
-var analyzeRecipeRequest = {"title":"Spaghetti Carbonara","servings":2,"ingredients":["1 lb spaghetti","3.5 oz pancetta","2 Tbsps olive oil","1  egg","0.5 cup parmesan cheese"],"instructions":"Bring a large pot of water to a boil and season generously with salt. Add the pasta to the water once boiling and cook until al dente. Reserve 2 cups of cooking water and drain the pasta. "}; // {AnalyzeRecipeRequest} Example request body.
-var opts = {
-  'language': "en", // {String} The input language, either \"en\" or \"de\".
-  'includeNutrition': false, // {Boolean} Whether nutrition data should be added to correctly parsed ingredients.
-  'includeTaste': false // {Boolean} Whether taste data should be added to correctly parsed ingredients.
+// help with spoonacular installation https://github.com/ddsky/spoonacular-api-clients/tree/master/javascript#installation
+
+// Configure API key authorization: apiKeyScheme
+let apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
+apiKeyScheme.apiKey = '6ec4ef0ad27443c593c8c9ba152ca71d';
+// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+//apiKeyScheme.apiKeyPrefix = 'Token';
+
+let apiInstance = new Spoonacular.RecipesApi();
+let query = "tofu"; // String | The (natural language) search query.
+let opts = {
+  'minIron': 0, // Number | The minimum amount of iron in milligrams the recipe must have.
 };
-var callback = function(error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('API called successfully. Returned data: ' + data);
-  }
-};
-api.analyzeRecipe(analyzeRecipeRequest, opts, callback);
+apiInstance.searchRecipes(query, opts, (error, data, response) => {
+    if (error) {
+      console.error('Error:', error);
+    } else {
+      console.log('API called successfully. Returned data:');
+      console.log(JSON.stringify(data, null, 2)); // Pretty-print with indentation
+    }
+  });
+  
