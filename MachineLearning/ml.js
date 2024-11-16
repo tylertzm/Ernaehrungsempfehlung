65ab74d68cf7c8fb5cd65b5f3c391c2e34b22c5a
tylertzm <TZM2002@protonmail.com>
2024-11-16 21:35:51 +0100

    first

--- MachineLearning/ml.js
+++
@@ -1,4 +0,0 @@
-// Food Recognition https://www.kaggle.com/code/bebofekry/101-food-classification
-// https://bildagentur.panthermedia.net/suche/medien/?q=avocado potential source for food images 
-// http://www.vegetarische-rezepte.com/vegetarischerezepte/auflauf/index.php zutaten, rezept, zubereitung
-// https://shop.rewe.de/api/products/?search= rewe api search
