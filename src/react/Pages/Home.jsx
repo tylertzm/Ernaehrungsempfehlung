9d44ec532510952fdeb64a442d5bfce9d368e966
Zhen.Tan <TZM2002@protonmail.com>
2025-01-25 01:03:01 +0100

    75%

--- src/react/Pages/Home.jsx
+++ src/react/Pages/Home.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 
 import { Stack, Typography } from '@mui/material'
-import DateCalendarFormProps from '../Components/Calendar'
+import dayjs from 'dayjs'
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'
 
 const Home = () => (
   <Stack
-    flex="1 1 auto"
-    justifyContent="center"
-    alignItems="center"
+    sx={{
+      width: '100%',
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 2
+    }}
   >
+
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoItem>
+        <DateCalendar defaultValue={dayjs('2025-01-17')} />
+      </DemoItem>
+  </LocalizationProvider>
+
     <Typography
       variant="h4"
     >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-      incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+
     </Typography>
   </Stack>
 )
